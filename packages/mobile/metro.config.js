9821c4b47e15244673b86c590972278b474a3042
Mateus <mateusbpts@gmail.com>
2020-07-24 13:10:27 -0300

    Mobile

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,21 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const {resolve} = require('path');
+
+module.exports = {
+  projectRoot: resolve(__dirname, '.'),
+  watchFolders: [resolve(__dirname, '../../node_modules')],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
